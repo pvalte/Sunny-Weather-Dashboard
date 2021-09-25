0db26c1d0b98eaf1bd72648a2bd7bcaaf3c3e598
Paige A <pvaltemare@gmail.com>
2021-09-24 17:44:13 -0700

    set up to fill in data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,35 @@ var searchFormEl = document.querySelector("#search-form");
 var savedCityButtonsEl = document.querySelector("#saved-cities");
 var cityInputEl = document.querySelector("#city");
 
+var cityNameTitleEl = document.querySelector("#city-name");
+var todayDateEl = document.querySelector("#todays-date");
+
 var loadSavedCities = function () {
-    cityArray = localStorage.getItem("cityList").split(",");
-    for (var i=0; i<cityArray.length; i++){
-        createSavedCityEl(cityArray[i]);
-    }
+    var cityList = localStorage.getItem("cityList");
+    if (cityList)
+        cityArray = cityList.split(",");
+        for (var i=0; i<cityArray.length; i++){
+            createSavedCityEl(cityArray[i]);
+        }
 }
 
 var createSavedCityEl = function (cityName) {
     var cityButtonEl = document.createElement("button");
     cityButtonEl.textContent = cityName;
     cityButtonEl.className = "saved-city btn col-12";
-    cityButtonEl.setAttribute("data-name", cityName);
     savedCityButtonsEl.appendChild(cityButtonEl);
 };
 
 
+var createTodaysForecast = function(city) {
+    cityNameTitleEl.textContent = city;
+    console.log(moment().format("MM/DD/YYYY"));
+    todayDateEl.textContent = moment().format("MM/DD/YYYY");
+};
+
+
 var formSubmitHandler = function (event) {
-    //event.preventDefault();
+    event.preventDefault();
     var newCity = cityInputEl.value.trim();
 
     if (newCity) {
@@ -31,12 +42,22 @@ var formSubmitHandler = function (event) {
         localStorage.setItem("cityList", cityArray);
         cityInputEl.value = "";
         createSavedCityEl(newCity);
+        createTodaysForecast(newCity);
     } else {
         alert("Please enter a city");
     }
 };
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
+
+var cityButtonHandler = function (event) {
+    //event.preventDefault();
+    var cityEl = event.target;
+    var cityName = cityEl.textContent;
+    createTodaysForecast(cityName);
+};
+
+savedCityButtonsEl.addEventListener("click", cityButtonHandler);
 
 var getWeather = function(lat, lon) {
     // format the github api url
@@ -62,4 +83,4 @@ var getWeather = function(lat, lon) {
 
 
 // getWeather(lat,lon);
-loadSavedCities()
+loadSavedCities();
